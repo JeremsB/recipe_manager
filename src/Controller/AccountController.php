09d6f6518b621f9d793d758fe8207f0c6c6e6868
Emilien <emilien.gantois.pro@gmail.com>
2020-04-06 04:51:04 +0200

    profile modification form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\AccountType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -73,6 +74,34 @@ class AccountController extends AbstractController
         }
 
         return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Affiche le profil de l'utilisateur
+     * 
+     * @Route("/account/profile", name="account_profile")
+     *
+     * @return Response
+     */
+    public function profile(Request $request, EntityManagerInterface $manager) {
+        $user = $this->getUser();
+
+        $form = $this->createForm(AccountType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les données du profil ont bien été sauvegardées !"
+            );
+        }
+
+        return $this->render('account/profile.html.twig', [
             'form' => $form->createView()
         ]);
     }
