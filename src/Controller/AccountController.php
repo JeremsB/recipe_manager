2692e16db460cd7247415cc59d8e0995e421dbc3
Emilien <emilien.gantois.pro@gmail.com>
2020-04-06 05:09:49 +0200

    change password form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,14 +4,17 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\AccountType;
+use App\Entity\PasswordUpdate;
 use App\Form\RegistrationType;
+use App\Form\PasswordUpdateType;
+use Symfony\Component\Form\FormError;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
@@ -102,6 +105,43 @@ class AccountController extends AbstractController
         }
 
         return $this->render('account/profile.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Affiche le formulaire de modification de mot de passe
+     * 
+     * @Route("/account/password", name="account_password")
+     *
+     * @return Response
+     */
+    public function updatePassword(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder) {
+        $passwordUpdate = new PasswordUpdate();
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!password_verify($passwordUpdate->getOldPassword(), $user->getHash())) {
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe saisie est différent de votre mot de passe actuel !"));
+            }
+            else {
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+
+                $user->setHash($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+            }
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('account/password.html.twig', [
+            'diary' => false,
             'form' => $form->createView()
         ]);
     }
