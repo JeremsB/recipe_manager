58e2e121ec2caf64d62a18529a4e0ad4ea83084b
Emilien <emilien.gantois.pro@gmail.com>
2020-05-29 17:03:03 +0200

    last modifications

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,7 +6,9 @@ use Faker\Factory;
 use App\Entity\User;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
+use App\Entity\Mark;
 use App\Entity\RecipeImage;
+use App\Repository\UserRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -14,19 +16,34 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class AppFixtures extends Fixture
 {
     private $encoder;
+    private $userRepo;
 
-    public function __construct(UserPasswordEncoderInterface $encoder) {
+    public function __construct(UserPasswordEncoderInterface $encoder, UserRepository $userRepo) {
         $this->encoder = $encoder;
+        $this->userRepo = $userRepo;
     }
 
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr-FR');
 
-        // Nous gérons les users
+        // Mise en place des utilisateurs
         $users = [];
 
-        for ($i=0; $i < 10; $i++) { 
+        // Création de l'utilisateur de test
+        $user = new User();
+
+        $user->setPseudo("Guillaume Ballas");
+        $user->setEmail("guillaume.ballas@persistance.com");
+
+        $hash = $this->encoder->encodePassword($user, 'password');
+        $user->setHash($hash);
+
+        $manager->persist($user);
+        $users[] = $user; 
+
+        // Création des autres utilisateurs
+        for ($i=0; $i < 2; $i++) { 
             $user = new User();
 
             $user->setPseudo($faker->firstname);
@@ -38,64 +55,97 @@ class AppFixtures extends Fixture
             $manager->persist($user);
             $users[] = $user; 
         }
-
-        // Nous gérons les ingrédients
+        // Mise en place de 5 ingrédients pour chaque utilisateur
+        $nameIngredients = [
+            'Chocolat', 'Oeuf', 'Poulet', 'Menthe', 'Lait', 'Laitue'
+        ];
         $ingredients = [];
 
-        for ($i=0; $i < 25; $i++) { 
-            $ingredient = new Ingredient();
+        foreach ($users as $user) {
+            foreach ($nameIngredients as $nameIngredient) {
+                $ingredient = new Ingredient();
 
-            $randIndex = array_rand($users);
-            $ingredient->setUser($users[$randIndex]);
+                $ingredient->setUser($user);
+                $ingredient->setName($nameIngredient);
+                $ingredient->setPrice(mt_rand(3,27));
 
-            $ingredient->setName("Name${i}");
-            $ingredient->setPrice(mt_rand(3,27));
-
-            $manager->persist($ingredient);
-            $ingredients[] = $ingredient;
-        }
-
-        // Nous gérons les recettes
-        $recipes = [];
-
-        for ($i=0; $i < 5; $i++) { 
-            $recipe = new Recipe();
-
-            $randIndex = array_rand($users);
-            $recipe->setUser($users[$randIndex]);
-
-            $recipe->setName("Recipe${i}");
-            $recipe->setInstructions($faker->text(50));
-            $recipe->setTime(mt_rand(5, 100));
-            $recipe->setDifficulty(mt_rand(1, 5));
-            $recipe->setPrice(mt_rand(70, 160) / 10);
-
-            $rand = mt_rand(0, 1);
-            $recipe->setShared($rand == 0 ? true : false);
-
-            $manager->persist($recipe);
-            $recipes[] = $recipe;
-
-            // Nous gérons les images liées à la recette
-            $recipeImages = [];
-            $rand = mt_rand(0, 5);
-
-            for ($i=0; $i < $rand; $i++) { 
-                $recipeImage = new RecipeImage();
-
-                $recipeImage->setUrl("https://i.picsum.photos/id/581/300/300.jpg");
-                $manager->persist($recipeImage); 
-                $recipeImages[] = $recipeImage;
-
-                $recipe->addImage($recipeImage);
+                $manager->persist($ingredient);
+                $ingredients[] = $ingredient;
             }
         }
 
-        // Nous gérons les ingrédients composants les recettes
+        // Mise en place de 3 recettes par utilisateur
+        $nameRecipes = [
+            0 => [
+                'Poulet au chocolat', 'Oeuf et menthe', 'Un peu de laitue et de lait'
+            ],
+            1 => [
+                'Poulet au lait', 'Oeuf au chocolat', 'Laitue et sa menthe'
+            ],
+            2 => [
+                'Poulet et sa laitue', 'Du lait et des oeufs', 'Chocolat a la menthe'
+                ]
+        ];
+        $recipes = [];
+
+        foreach ($users as $userIndex => $user) {            
+            foreach ($nameRecipes as $nameRecipe) {
+                $recipe = new Recipe();
+
+                $recipe->setUser($user);
+                $recipe->setName($nameRecipe[$userIndex]);
+                $recipe->setInstructions($faker->text(50));
+                $recipe->setTime(mt_rand(5, 80));
+                $recipe->setDifficulty(mt_rand(1, 5));
+                $recipe->setPrice(mt_rand(70, 160) / 10);
+
+                $rand = mt_rand(0, 1);
+                $recipe->setShared($rand == 0 ? true : false);
+
+                $manager->persist($recipe);
+                $recipes[] = $recipe;
+                $userRecipes[] = $recipe;
+
+                // Nous gérons les images liées à la recette
+                $recipeImages = [];
+                $rand = mt_rand(0, 5);
+
+                for ($i=0; $i < $rand; $i++) { 
+                    $recipeImage = new RecipeImage();
+
+                    $recipeImage->setUrl("https://i.picsum.photos/id/581/300/300.jpg");
+                    $manager->persist($recipeImage); 
+                    $recipeImages[] = $recipeImage;
+
+                    $recipe->addImage($recipeImage);
+                }
+            }
+        }
+
+        // Mise en place des ingrédients pour chaque recette
         foreach ($recipes as $recipe) {
             foreach ($ingredients as $ingredient) {
-                $rand = mt_rand(0, 1);
-                $rand == 0 ? $recipe->addIngredient($ingredient) : '';
+                if($recipe->getUser() === $ingredient->getUser()) {
+                    $rand = mt_rand(0, 1);
+                    $rand == 0 ? $recipe->addIngredient($ingredient) : '';
+                }
+            }
+        }
+
+        // Mise en place des ingrédients pour chaque recette
+        foreach ($recipes as $recipe) {
+            if($recipe->getShared()) {
+                $user = $users[array_rand($users)];
+                
+                if($user !== $recipe->getUser()) {
+                    $mark = new Mark();
+
+                    $mark->setMark(mt_rand(0, 10));
+                    $mark->setUser($user);
+                    $mark->setRecipe($recipe);
+
+                    $manager->persist($mark);
+                }
             }
         }
         
